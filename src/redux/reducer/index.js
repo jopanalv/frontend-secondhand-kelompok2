b49fbab1ad4cc1b7004f2067802e336969267c28
rtsania <tsania.salsabila3@gmail.com>
2022-07-06 23:23:56 +0700

    login initial commit

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import RegisterReducer from "./Register";
 import ProfileReducer from "./Profile";
+import LoginReducer from "./Login";
+import AddProductReducer from "./AddProduct";
 
 export default combineReducers({
   ProfileReducer,
   RegisterReducer,
+  LoginReducer,
+  AddProductReducer,
 });
--- src/redux/type.js
+++ src/redux/type.js
@@ -1,3 +1,9 @@
 export const ADD_REGISTER = "ADD_REGISTER";
 
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+export const LOGIN_ERROR = "LOGIN_ERROR";
+
 export const UPDATE_PROFILE = "UPDATE_PROFILE";
+
+export const ADD_PRODUCT = "ADD_PRODUCT";
