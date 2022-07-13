0ac275e48c3b70a1e36dec29d3623206db580cb6
jopanalv <jopan.alv@gmail.com>
2022-07-13 19:48:18 +0700

    refactor add product

--- src/pages/DafJual.js
+++ src/pages/DafJual.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
-import Navbar from '../component/Navbar';
+import { useSelector } from 'react-redux';
+import Navbar from '../component/Navbar1';
+import LogedNavbar from '../component/Navbar';
 import Notif from '../component/Notif';
 import Alert from '../component/Alert';
 import Penjual from '../component/Penjual';
 import Jual from '../component/Daftar-jual';
 
 export const DafJual = () => {
+    const auth = useSelector(state => state.login)
+
     return (
         <div>
-            <Navbar />
+            {
+                auth.isAuthenticated ? <LogedNavbar /> : <Navbar />
+            }
             <Notif />
             <Alert />
             <Penjual />
